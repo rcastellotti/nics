bcde4c338b4a17e0bada3856a0f14b9eaad8010f
Roberto Castellotti <me@rcastellotti.dev>
2026-03-01 19:20:07 +0100

    Set hostname for rcastellotti-dev configuration

--- rcastellotti-dev/configuration.nix
+++ rcastellotti-dev/configuration.nix
@@ -11,6 +11,7 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
+  networking.hostName = "rcastellotti-dev";
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
