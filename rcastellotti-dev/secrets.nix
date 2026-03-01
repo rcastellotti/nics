36210d752a585a73affbe13b2e5726d6cc213ef9
Roberto Castellotti <me@rcastellotti.dev>
2026-03-01 19:19:25 +0100

    Replace yubikey with key for token public keys

--- rcastellotti-dev/secrets.nix
+++ rcastellotti-dev/secrets.nix
@@ -1,7 +1,7 @@
 let
-  yubikey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqVLRRlGF1nezM9nM87dUBkp3hKkDB+yqJyqPVwt2Wg";
+  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILqVLRRlGF1nezM9nM87dUBkp3hKkDB+yqJyqPVwt2Wg";
 in
 {
-  "HCLOUD_TOKEN.age".publicKeys = [ yubikey ];
-  "CLOUDFLARE_API_TOKEN.age".publicKeys = [ yubikey ];
+  "HCLOUD_TOKEN.age".publicKeys = [ key ];
+  "CLOUDFLARE_API_TOKEN.age".publicKeys = [ key ];
 }
