00860ede31efc9e53ae56940a18e0adeccf84733
Roberto Castellotti <me@rcastellotti.dev>
2026-03-01 20:08:05 +0100

    rcastellotti-dev: update config

--- rcastellotti-dev/configuration.nix
+++ rcastellotti-dev/configuration.nix
@@ -8,10 +8,25 @@
 
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    ./hardware-configuration.nix
+    ./disko-config.nix
   ];
 
   networking.hostName = "rcastellotti-dev";
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  users.users.rc = {
+    isNormalUser = true;
+    description = "rc";
+    initialHashedPassword = "$y$j9T$17wh/3yJrjKDieCQllkEM0$tPKMhABvPxNGJkcvzMI26pqhxRVKs3TtTMD2pHBN0b3";
+    extraGroups = [
+      "wheel"
+    ];
+    packages = with pkgs; [
+      nushell
+      tailscale
+    ];
+  };
+  system.stateVersion = "25.11";
 }
