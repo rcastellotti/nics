d1e06936bd7d808daa376a953e3521df9d2c02a5
Roberto Castellotti <me@rcastellotti.dev>
2026-03-03 15:13:21 +0100

    Update deploy script for SSH key retrieval

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# https://nix-community.github.io/nixos-anywhere/howtos/secrets.html
 temp=$(mktemp -d)
 
 cleanup() {
@@ -8,9 +8,7 @@ cleanup() {
 trap cleanup EXIT
 
 install -d -m755 "$temp/etc/ssh"
-
 bw get item $(bw list items --search "rc-ssh-key" | jq -r '.[].id') | jq -r '.sshKey.privateKey' > "$temp/etc/ssh/ssh_host_ed25519_key"
-
 chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
 
 nix run github:nix-community/nixos-anywhere -- --flake .#rcastellotti-dev --target-host root@89.167.105.83 --build-on-remote -i /tmp/rc-ssh-key --extra-files "$temp"
